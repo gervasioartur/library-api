3ef2f93f3da661fe0eec41cdff26e20fcc461b38
Gervasio Artur <gervasioartur@outlook.com.br>
2023-02-20 23:16:09 -0300

    test: ensure BookRepository returns true on save sucess

---
+++ src/test/java/com/gervasioartur/libraryapi/mode/repository/BookRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.gervasioartur.libraryapi.mode.repository;
+
+import com.gervasioartur.libraryapi.model.entity.Book;
+import com.gervasioartur.libraryapi.model.repository.BookRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@DataJpaTest
+public class BookRepositoryTest {
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    @DisplayName("Should return true on save success")
+    public void returnTrueIfIsbnExistTest() {
+        String isbn = "123";
+        entityManager.persist(Book.builder().author("Gerry").title("gerry").isbn(isbn).build());
+        boolean exists = bookRepository.existsByIsbn(isbn);
+        assertThat(exists).isTrue();
+    }
+}
