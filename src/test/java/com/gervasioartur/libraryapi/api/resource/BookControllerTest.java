8cb4bb6607836c0f1d43a5ba2fdf1cea0b479f15
Gervasio Artur <gervasioartur@outlook.com.br>
2023-02-20 21:52:59 -0300

    test: ensure BookController throws BusinesException if the Book isbn alread exists

--- src/test/java/com/gervasioartur/libraryapi/api/resource/BookControllerTest.java
+++ src/test/java/com/gervasioartur/libraryapi/api/resource/BookControllerTest.java
@@ -2,6 +2,7 @@ package com.gervasioartur.libraryapi.api.resource;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gervasioartur.libraryapi.api.dto.BookDTO;
+import com.gervasioartur.libraryapi.exception.BusinessException;
 import com.gervasioartur.libraryapi.model.entity.Book;
 import com.gervasioartur.libraryapi.service.BookService;
 import org.junit.jupiter.api.DisplayName;
@@ -77,5 +78,27 @@ public class BookControllerTest {
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("errors", hasSize(3)));
 
+    }
+
+    @Test
+    @DisplayName("Should throw an error if the book isbn is already used")
+    public void creatBookWithDuplicatedIsbnTest() throws Exception {
+        String errorMessage = "isbn already used";
+        BookDTO bookDTO = BookDTO.builder().author("Artur").title("As aventuras").isbn("001").build();
+        String json = new ObjectMapper().writeValueAsString(bookDTO);
+        BDDMockito.given(bookService.save(Mockito.any(Book.class)))
+                .willThrow(new BusinessException(errorMessage));
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(BOOK_API)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mvc
+                .perform(request)
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("errors", hasSize(1)))
+                .andExpect(jsonPath("errors[0]").value(errorMessage));
     }
 }
