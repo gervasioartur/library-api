d893dc12c348043e22248399e4425890b43ec901
Gervasio Artur <gervasioartur@outlook.com.br>
2023-02-23 02:30:46 -0300

    feat: ensure BookController returns resource not found if the book does not existis

--- src/main/java/com/gervasioartur/libraryapi/api/resource/BookController.java
+++ src/main/java/com/gervasioartur/libraryapi/api/resource/BookController.java
@@ -5,17 +5,15 @@ import com.gervasioartur.libraryapi.api.exception.ApiErros;
 import com.gervasioartur.libraryapi.exception.BusinessException;
 import com.gervasioartur.libraryapi.model.entity.Book;
 import com.gervasioartur.libraryapi.service.BookService;
-import lombok.Getter;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
@@ -36,20 +34,22 @@ public class BookController {
 
     @GetMapping("{id}")
     public BookDTO getOne(@PathVariable Long id) {
-       Book book = bookService.getById(id).get();
-       return modelMapper.map(book, BookDTO.class);
+        return bookService
+                .getById(id)
+                .map(book -> modelMapper.map(book, BookDTO.class))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public  ApiErros handleValidationException(MethodArgumentNotValidException exception){
+    public ApiErros handleValidationException(MethodArgumentNotValidException exception) {
         BindingResult bindingResult = exception.getBindingResult();
-        return  new ApiErros(bindingResult);
+        return new ApiErros(bindingResult);
     }
 
     @ExceptionHandler(BusinessException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public  ApiErros handleBusinessException(BusinessException exception){
-        return  new ApiErros(exception);
+    public ApiErros handleBusinessException(BusinessException exception) {
+        return new ApiErros(exception);
     }
 }
