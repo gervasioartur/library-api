5a33d43d683af91fed8bbc65666eb0d73e61a79f
Gervasio Artur <gervasioartur@outlook.com.br>
2023-03-21 21:55:05 -0300

    test: ensure BookRepository returns if getById succeeds

--- src/test/java/com/gervasioartur/libraryapi/mode/repository/BookRepositoryTest.java
+++ src/test/java/com/gervasioartur/libraryapi/mode/repository/BookRepositoryTest.java
@@ -11,6 +11,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
@@ -23,20 +25,34 @@ public class BookRepositoryTest {
     @Autowired
     BookRepository bookRepository;
 
+    private Book bookFactory ( ){
+        Book book = Book.builder().author("Gerry").title("gerry").isbn("123").build();
+        return book;
+    }
+
     @Test
     @DisplayName("Should return true on save success")
     public void returnTrueIfIsbnExistTest() {
         String isbn = "123";
-        entityManager.persist(Book.builder().author("Gerry").title("gerry").isbn(isbn).build());
+        entityManager.persist(this.bookFactory());
         boolean exists = bookRepository.existsByIsbn(isbn);
         assertThat(exists).isTrue();
     }
 
     @Test
-    @DisplayName("Should return true on save failure")
+    @DisplayName("Should return false on save failure")
     public void returnFalseIfIsbnExistTest() {
         String isbn = "123";
         boolean exists = bookRepository.existsByIsbn(isbn);
         assertThat(exists).isFalse();
+    }
+
+    @Test
+    @DisplayName("Should return true on getById success")
+    public  void getByIdTest() {
+        Book book = this.bookFactory();
+        entityManager.persist(book);
+        Optional<Book> foundBook = bookRepository.findById(book.getId());
+        assertThat(foundBook.isPresent()).isTrue();
     }
 }
