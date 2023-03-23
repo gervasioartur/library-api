db245ab22e1a35191a15374dfa21750160e0d7dc
Gervasio Artur <gervasioartur@outlook.com.br>
2023-03-22 22:07:55 -0300

    test: ensure BookService returns error if book not exists

--- src/test/java/com/gervasioartur/libraryapi/service/BookServiceTest.java
+++ src/test/java/com/gervasioartur/libraryapi/service/BookServiceTest.java
@@ -5,6 +5,7 @@ import com.gervasioartur.libraryapi.exception.BusinessException;
 import com.gervasioartur.libraryapi.model.entity.Book;
 import com.gervasioartur.libraryapi.model.repository.BookRepository;
 import com.gervasioartur.libraryapi.service.impl.BookServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -88,6 +89,11 @@ public class BookServiceTest {
         assertThat(book.getAuthor()).isEqualTo(updatedBook.getAuthor());
     }
 
-
-
+    @Test
+    @DisplayName("Should return an error if when trying to update invalid book")
+    public void updateInvalidBookTest(){
+        Book book = new Book();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> bookService.update(book));
+        Mockito.verify( bookRepository, Mockito.never() ).save(book);
+    }
 }
