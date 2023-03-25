1ead65347ce04d3f8800230a91d180e26b81ae5c
Gervasio Artur <gervasioartur@outlook.com.br>
2023-03-25 17:43:18 -0300

    test: ensure LoanController leans a book

---
+++ src/test/java/com/gervasioartur/libraryapi/api/resource/LoanControllerTest.java
@@ -0,0 +1,66 @@
+package com.gervasioartur.libraryapi.api.resource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gervasioartur.libraryapi.api.dto.LoanDTO;
+import com.gervasioartur.libraryapi.model.entity.Book;
+import com.gervasioartur.libraryapi.model.entity.Loan;
+import com.gervasioartur.libraryapi.service.BookService;
+import com.gervasioartur.libraryapi.service.LoanService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest(controllers = LoanController.class)
+@AutoConfigureMockMvc
+public class LoanControllerTest {
+    static final String LOAN_API = "/api/loans";
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    BookService bookService;
+
+    @MockBean
+    LoanService loanService;
+
+    @Test
+    @DisplayName("Should loan a book")
+    public void createLoanTest( )throws Exception {
+        LoanDTO dto =  new LoanDTO().builder().isbn("123").customer("Fulano").build();
+        String json =  new ObjectMapper().writeValueAsString(dto);
+
+        Book book = Book.builder().id(1l).isbn("123").build();
+        BDDMockito.given(bookService.getBookByIsbn("123")).willReturn(Optional.of(book));
+
+        Loan loan = Loan.builder().id(1l).customer("Fulano").book(book).loanDate(LocalDate.now()).build();
+        BDDMockito.given(loanService.save(Mockito.any(Loan.class))).willReturn(loan);
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(LOAN_API)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mvc
+                .perform(request)
+                .andExpect(status().isCreated())
+                .andExpect(content().string("1"));
+    }
+}
