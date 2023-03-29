cc084e2f6d53d491d4f1bfad79413ba059b3bb86
Gervasio Artur <gervasioartur@outlook.com.br>
2023-03-29 01:13:57 -0300

    feat: ensure LoanRepository returns a loan tha was not returned for a book

--- src/main/java/com/gervasioartur/libraryapi/model/repository/LoanRepository.java
+++ src/main/java/com/gervasioartur/libraryapi/model/repository/LoanRepository.java
@@ -3,7 +3,11 @@ package com.gervasioartur.libraryapi.model.repository;
 import com.gervasioartur.libraryapi.model.entity.Book;
 import com.gervasioartur.libraryapi.model.entity.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LoanRepository extends JpaRepository<Loan, Long> {
-    boolean existsByBookAndNotReturned(Book book);
+    @Query(value = " select case when (count(l.id) > 0) then true else false end " +
+            "from Loan l where  l.book= :book and l.returned <> true ")
+    boolean existsByBookAndNotReturned(@Param("book") Book book);
 }
