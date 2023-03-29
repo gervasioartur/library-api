0685e12a642c61870b56a75439fb100cac50f16e
Gervasio Artur <gervasioartur@outlook.com.br>
2023-03-28 21:57:00 -0300

    feat: ensure LoanController returns an error when trying to loan an inexistent book

--- src/main/java/com/gervasioartur/libraryapi/api/resource/LoanController.java
+++ src/main/java/com/gervasioartur/libraryapi/api/resource/LoanController.java
@@ -8,6 +8,7 @@ import com.gervasioartur.libraryapi.service.LoanService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 
@@ -21,7 +22,11 @@ public class LoanController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Long create(@RequestBody LoanDTO loanDTO ){
-        Book book = bookService.getBookByIsbn(loanDTO.getIsbn()).get();
+        Book book = bookService
+                .getBookByIsbn(loanDTO.getIsbn())
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Book not found for passed isbn!"));
+
         Loan loan = Loan
                     .builder()
                     .book(book)
