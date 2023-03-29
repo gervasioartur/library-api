459fe866737752349c6cbcf26353d8dc2edd72ac
Gervasio Artur <gervasioartur@outlook.com.br>
2023-03-28 22:28:06 -0300

    feat: ensure BookService returns a book by isbn

--- src/main/java/com/gervasioartur/libraryapi/model/repository/BookRepository.java
+++ src/main/java/com/gervasioartur/libraryapi/model/repository/BookRepository.java
@@ -3,7 +3,10 @@ package com.gervasioartur.libraryapi.model.repository;
 import com.gervasioartur.libraryapi.model.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
     boolean existsByIsbn(String isbn);
 
+    Optional<Book> findByIsbn(String isbn);
 }
