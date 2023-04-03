f2d9cbe900b2e26a63f632467879b4d584c62f3a
Gervasio Artur <gervasioartur@outlook.com.br>
2023-04-03 20:42:24 -0300

    feat: ensure LoanController filters loans

--- src/main/java/com/gervasioartur/libraryapi/api/resource/LoanController.java
+++ src/main/java/com/gervasioartur/libraryapi/api/resource/LoanController.java
@@ -7,43 +7,63 @@ import com.gervasioartur.libraryapi.model.entity.Loan;
 import com.gervasioartur.libraryapi.service.BookService;
 import com.gervasioartur.libraryapi.service.LoanService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/loans")
 @RequiredArgsConstructor
 public class LoanController {
     private final LoanService loanService;
+    @Autowired
     private final BookService bookService;
+    @Autowired
+    private final ModelMapper modelMapper;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Long create(@RequestBody LoanDTO loanDTO ){
+    public Long create(@RequestBody LoanDTO loanDTO) {
         Book book = bookService
                 .getBookByIsbn(loanDTO.getIsbn())
                 .orElseThrow(
                         () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Book not found for passed isbn!"));
 
         Loan loan = Loan
-                    .builder()
-                    .book(book)
-                    .customer(loanDTO.getCustomer())
-                    .loanDate(LocalDate.now())
-                    .build();
-        loan =  loanService.save(loan);
+                .builder()
+                .book(book)
+                .customer(loanDTO.getCustomer())
+                .loanDate(LocalDate.now())
+                .build();
+        loan = loanService.save(loan);
         return loan.getId();
     }
 
     @PatchMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void returnedBook (@PathVariable long id, @RequestBody ReturnedLoanDTO returnedLoanDTO){
+    public void returnedBook(@PathVariable long id, @RequestBody ReturnedLoanDTO returnedLoanDTO) {
         Loan loan = this.loanService.getById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         loan.setReturned(returnedLoanDTO.getReturned());
         this.loanService.update(loan);
     }
 
+    @GetMapping
+    public Page<LoanDTO> find(LoanDTO loanDTO, Pageable pageRequest) {
+        Loan filter = modelMapper.map(loanDTO, Loan.class);
+        Page<Loan> result = loanService.find(filter, pageRequest);
+        List<LoanDTO> list = result.getContent()
+                .stream()
+                .map(entity -> modelMapper.map(entity, LoanDTO.class))
+                .collect(Collectors.toList());
+        return new PageImpl<LoanDTO>(list, pageRequest, result.getTotalElements());
+    }
 }
